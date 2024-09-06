a93f963b327abcc35663d9e5182b9f7d32d187b1
ByteYue <yj976240184@gmail.com>
2024-09-06 16:41:11 +0800

    ok

--- deploy_utils/start.sh
+++ deploy_utils/start.sh
@@ -4,7 +4,7 @@ echo ${ROOT_PATH}
 log_suffix=$(date +"%Y-%d-%m:%H:%M:%S")
 
 function start_node() {
-    node_path=$1
+    cur_node_path=$1
     port=$2
     node_path=${ROOT_PATH}/${node_path}
     rm -rf $node_path/data/quorumstoreDB
@@ -14,7 +14,7 @@ function start_node() {
     jq 'walk(if type == "object" and (.last_voted_round? // .highest_timeout_round? // .preferred_round? // .one_chain_round?) then .last_voted_round |= 0 | .preferred_round |= 0 | .one_chain_round |= 0 | .highest_timeout_round |= 0 else . end)' ${node_path}/data/secure_storage.json > secure_storage.json
     mv secure_storage.json ${node_path}/data/
     pid=$(${ROOT_PATH}/target/debug/gravity_consensus --data-path ${node_path}/data --port ${port} --discovery-path ${node_path}/discovery > logs/${port}.${log_suffix}.log & echo $!)
-    echo $pid > ${node_path}.pid
+    echo $pid > ${cur_node_path}.pid
 }
 
 node_arg=$1
