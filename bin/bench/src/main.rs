f1a45ffe28c5cab82c20a170b19b2f57692e791b
ByteYue <yj976240184@gmail.com>
2024-12-17 15:47:58 +0800

    Revert "m"
    This reverts commit 1f681275a228dee936bbe984cfd22028a466ae00.

--- bin/bench/src/main.rs
+++ bin/bench/src/main.rs
@@ -11,8 +11,7 @@ use api_types::{
 };
 use clap::Parser;
 use cli::Cli;
-use flexi_logger::{Logger, FileSpec, WriteMode, DeferredNow, Record};
-use std::io::Write;
+use flexi_logger::{FileSpec, Logger, WriteMode};
 use kv::KvStore;
 use log::info;
 use once_cell::sync::OnceCell;
@@ -109,22 +108,6 @@ fn generate_random_address() -> ExternalAccountAddress {
     ExternalAccountAddress::new(random_bytes)
 }
 
-fn custom_format(
-    writer: &mut dyn Write,
-    now: &mut DeferredNow,
-    record: &Record,
-) -> std::io::Result<()> {
-    use chrono::Local;
-    write!(
-        writer,
-        "{:?} [{}] {}: {}\n",
-        Local::now().format("%Y-%m-%d %H:%M:%S"),
-        record.module_path().unwrap_or("<unnamed>"),
-        record.level(),
-        &record.args()
-    )
-}
-
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
@@ -132,7 +115,6 @@ async fn main() {
     Logger::try_with_str("info")
         .unwrap()
         .log_to_file(FileSpec::default().directory(cli.log_dir.clone()))
-        .format(custom_format)
         .write_mode(WriteMode::BufferAndFlush)
         .start()
         .unwrap();
