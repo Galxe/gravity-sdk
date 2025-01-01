1de09ee03ebaf06190002d633bee7083faa24e06
kk-x <exmple@example.com>
2025-01-01 15:23:27 +0800

    construct pipeline apiv2

---
+++ bin/pethv2/src/reth_cli.rs
@@ -0,0 +1,331 @@
+use alloy_consensus::{TxEip1559, TxEip2930, TxLegacy};
+use alloy_primitives::{Address, B256};
+use alloy_rpc_types_engine::ForkchoiceState;
+use api_types::account::{ExternalAccountAddress, ExternalChainId};
+use api_types::u256_define::{BlockId as ExternalBlockId, TxnHash};
+use api_types::{simple_hash, ExternalBlock, VerifiedTxn, VerifiedTxnWithAccountSeqNum};
+use jsonrpsee::core::Serialize;
+use jsonrpsee::http_client::transport::HttpBackend;
+use jsonrpsee::http_client::HttpClient;
+use reth::rpc::builder::auth::AuthServerHandle;
+use reth_ethereum_engine_primitives::{EthEngineTypes, EthPayloadAttributes};
+use reth_payload_builder::PayloadId;
+use reth_pipe_exec_layer_ext_v2::{OrderedBlock, PipeExecLayerApi};
+use reth_primitives::{Bytes, Signature, TransactionSigned, TxKind, U256};
+use reth_rpc_api::{EngineApiClient, EngineEthApiClient};
+use reth_rpc_layer::AuthClientService;
+use reth_rpc_types::{AccessList, AccessListItem};
+use std::io::Read;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use tokio_stream::StreamExt;
+use tracing::info;
+use tracing::log::error;
+use web3::transports::Ipc;
+use web3::types::{BlockId, Transaction, TransactionId, H160};
+use web3::Web3;
+
+pub struct RethCli {
+    ipc: Web3<Ipc>,
+    auth: AuthServerHandle,
+    pipe_api: Mutex<PipeExecLayerApi>,
+    chain_id: u64,
+}
+
+pub fn covert_account(acc: H160) -> ExternalAccountAddress {
+    let mut bytes = [0u8; 32];
+    bytes[12..].copy_from_slice(acc.as_bytes());
+    ExternalAccountAddress::new(bytes)
+}
+
+impl RethCli {
+    fn create_payload_attributes(parent_beacon_block_root: B256, ts: u64) -> EthPayloadAttributes {
+        EthPayloadAttributes {
+            timestamp: ts,
+            prev_randao: B256::ZERO,
+            suggested_fee_recipient: Address::ZERO,
+            withdrawals: Some(Vec::new()),
+            parent_beacon_block_root: Some(parent_beacon_block_root),
+        }
+    }
+
+    pub async fn get_latest_block_hash(&self) -> Result<B256, String> {
+        let block = self.ipc.eth().block(BlockId::Number(web3::types::BlockNumber::Latest)).await;
+
+        match block {
+            Ok(Some(block)) => {
+                let hash = B256::from_slice(block.hash.unwrap().as_bytes());
+                Ok(hash)
+            }
+            _ => Err("Failed to get block".to_string()),
+        }
+    }
+
+    pub async fn new(ipc_url: &str, auth: AuthServerHandle, pipe_api: PipeExecLayerApi) -> Self {
+        let transport = web3::transports::Ipc::new(ipc_url).await.unwrap();
+        let ipc = web3::Web3::new(transport);
+        let chain_id = ipc.eth().chain_id().await.unwrap();
+        RethCli { ipc, auth, pipe_api: Mutex::new(pipe_api), chain_id: chain_id.as_u64() }
+    }
+
+    fn block_id_to_b256(block_id: ExternalBlockId) -> B256 {
+        B256::new(block_id.0)
+    }
+
+    fn construct_sig(txn: &Transaction) -> Signature {
+        let r = txn.r.unwrap();
+        let s = txn.s.unwrap();
+        let v = txn.v.unwrap();
+        let odd_y_parity = match v.as_u64() {
+            27 => false,
+            28 => true,
+            v => v % 2 == 1, // EIP-155 情况
+        };
+        let mut bytes = [0u8; 32];
+        r.to_big_endian(&mut bytes);
+        let r = U256::from_be_bytes(bytes);
+        let mut bytes = [0u8; 32];
+        s.to_big_endian(&mut bytes);
+        let s = U256::from_be_bytes(bytes);
+        Signature { r, s, odd_y_parity }
+    }
+
+    fn to_tx_kind(address: Option<H160>) -> TxKind {
+        match address {
+            Some(address) => TxKind::Call(Address::new(address.0)),
+            None => TxKind::Create,
+        }
+    }
+
+    fn convert_accest_list(access_list: Option<Vec<web3::types::AccessListItem>>) -> AccessList {
+        AccessList(
+            access_list
+                .unwrap_or_default()
+                .iter()
+                .map(|x| AccessListItem {
+                    address: Address::from_slice(x.address.as_bytes()),
+                    storage_keys: x
+                        .storage_keys
+                        .iter()
+                        .map(|x| B256::from_slice(x.as_bytes()))
+                        .collect(),
+                })
+                .collect(),
+        )
+    }
+
+    fn convert_to_reth_transaction(
+        tx: web3::types::Transaction,
+        chain_id: u64,
+    ) -> reth::primitives::Transaction {
+        match tx.transaction_type.map(|t| t.as_u64()) {
+            Some(0) => reth::primitives::Transaction::Legacy(TxLegacy {
+                chain_id: Some(chain_id),
+                nonce: tx.nonce.as_u64(),
+                gas_price: tx.gas_price.unwrap().as_u128(),
+                gas_limit: tx.gas.as_u128(),
+                to: Self::to_tx_kind(tx.to),
+                value: U256::from(tx.value.as_u128()),
+                input: Bytes::copy_from_slice(tx.input.0.as_slice()),
+            }),
+            Some(1) => reth::primitives::Transaction::Eip2930(TxEip2930 {
+                chain_id,
+                nonce: tx.nonce.as_u64(),
+                gas_price: tx.gas_price.unwrap().as_u128(),
+                gas_limit: tx.gas.as_u128(),
+                to: Self::to_tx_kind(tx.to),
+                value: U256::from(tx.value.as_u128()),
+                access_list: Self::convert_accest_list(tx.access_list),
+                input: Bytes::copy_from_slice(tx.input.0.as_slice()),
+            }),
+            Some(2) => reth::primitives::Transaction::Eip1559(TxEip1559 {
+                chain_id,
+                nonce: tx.nonce.as_u64(),
+                gas_limit: tx.gas.as_u128(),
+                to: Self::to_tx_kind(tx.to),
+                value: U256::from(tx.value.as_u128()),
+                access_list: Self::convert_accest_list(tx.access_list),
+                input: Bytes::copy_from_slice(tx.input.0.as_slice()),
+                max_fee_per_gas: tx.max_fee_per_gas.unwrap().as_u128(),
+                max_priority_fee_per_gas: tx.max_priority_fee_per_gas.unwrap().as_u128(),
+            }),
+            _ => panic!("Unknown transaction type {:?}", tx.transaction_type),
+        }
+    }
+
+    fn txn_to_signed(bytes: &[u8], chain_id: u64) -> (Address, TransactionSigned) {
+        let txn = serde_json::from_slice::<Transaction>(bytes).unwrap();
+        let address = txn.from.unwrap();
+        let address = Address::new(address.0);
+        let hash = txn.hash;
+        let hash = B256::from_slice(hash.as_bytes());
+        let signature = Self::construct_sig(&txn);
+        let transaction = Self::convert_to_reth_transaction(txn, chain_id);
+        info!("txn to signed {:?}", transaction);
+        info!("address {:?}", address);
+        (address, TransactionSigned { hash, signature, transaction })
+    }
+
+    pub async fn push_ordered_block(
+        &self,
+        block: ExternalBlock,
+        parent_hash: B256,
+    ) -> Result<PayloadId, String> {
+        // let pipe_api = self.pipe_api.lock().await;
+        // let payload_attr =
+        //     Self::create_payload_attributes(parent_hash.into(), block.block_meta.usecs / 1000000);
+        // let fcu_state = ForkchoiceState {
+        //     head_block_hash: parent_hash.into(),
+        //     safe_block_hash: parent_hash.into(),
+        //     finalized_block_hash: parent_hash.into(),
+        // };
+        // let engine_api = self.auth.http_client();
+        // let mut senders = vec![];
+        // let mut transactions = vec![];
+        // for (sender, txn) in
+        //     block.txns.iter().map(|txn| Self::txn_to_signed(&txn.bytes, self.chain_id))
+        // {
+        //     senders.push(sender);
+        //     transactions.push(txn);
+        // }
+        // pipe_api.push_ordered_block(OrderedBlock {
+        //     block_id: Self::block_id_to_b256(block.block_meta.block_id),
+        //     parent_hash: parent_hash.into(),
+        //     transactions,
+        //     senders,
+        // });
+        // let res = <HttpClient<AuthClientService<HttpBackend>> as EngineApiClient<EthEngineTypes>>::fork_choice_updated_v3(
+        //     &engine_api,
+        //     fcu_state, 
+        //     Some(payload_attr)
+        // ).await;
+        // match res {
+        //     Ok(res) => {
+        //         let payload_id = res.payload_id;
+        //         match payload_id {
+        //             Some(payload_id) => Ok(payload_id),
+        //             None => Err("Payload id not found".to_string()),
+        //         }
+        //     }
+        //     Err(_) => Err("Failed to push ordered block".to_string()),
+        // }
+        todo!()
+    }
+
+    pub async fn process_payload_id(
+        &self,
+        block_id: B256,
+        payload_id: PayloadId,
+    ) -> Result<B256, ()> {
+        // let mut pipe_api = self.pipe_api.lock().await;
+        // let block_hash = pipe_api.pull_executed_block_hash(payload_id, block_id).await.unwrap();
+        // Ok(block_hash)
+        todo!()
+    }
+
+    pub async fn commit_block(
+        &self,
+        parent_beacon_block_root: B256,
+        payload_id: PayloadId,
+        block_hash: B256,
+    ) -> Result<(), String> {
+        // let mut pipe_api = self.pipe_api.lock().await;
+        // pipe_api.ready_to_get_payload(payload_id).await.unwrap();
+        // let engine_api = self.auth.http_client();
+        // let payload = <HttpClient<AuthClientService<HttpBackend>> as EngineApiClient<
+        //     EthEngineTypes,
+        // >>::get_payload_v3(&engine_api, payload_id)
+        // .await
+        // .unwrap();
+        // assert_eq!(payload.execution_payload.payload_inner.payload_inner.block_hash, block_hash);
+        // pipe_api.ready_to_new_payload(block_hash.into()).await.unwrap();
+        // let res = <HttpClient<AuthClientService<HttpBackend>> as EngineApiClient<EthEngineTypes>>::new_payload_v3(
+        //     &engine_api,
+        //     payload.execution_payload,
+        //     vec![],
+        //     parent_beacon_block_root,
+        // ).await;
+        // info!("payload status {:?}", res);
+        // let res = <HttpClient<AuthClientService<HttpBackend>> as EngineApiClient<EthEngineTypes>>::fork_choice_updated_v3(
+        //     &engine_api,
+        //     ForkchoiceState {
+        //         head_block_hash: block_hash.into(),
+        //         safe_block_hash: block_hash.into(),
+        //         finalized_block_hash: block_hash.into(),
+        //     },
+        //     None
+        // ).await;
+        // match res {
+        //     Ok(_) => Ok(()),
+        //     Err(_) => Err("Failed to commit block".to_string()),
+        // }
+        todo!()
+    }
+
+    pub async fn process_pending_transactions(
+        &self,
+        buffer: Arc<Mutex<Vec<VerifiedTxnWithAccountSeqNum>>>,
+    ) -> Result<(), String> {
+        let mut eth_sub =
+            self.ipc.eth_subscribe().subscribe_new_pending_transactions().await.unwrap();
+        info!("start process pending transactions");
+        while let Some(Ok(txn_hash)) = eth_sub.next().await {
+            info!("get txn hash {:?}", txn_hash);
+            let txn = self.ipc.eth().transaction(TransactionId::Hash(txn_hash)).await;
+            info!("get txn {:?}", txn);
+            if let Ok(Some(txn)) = txn {
+                let account = match txn.from {
+                    Some(account) => account,
+                    None => {
+                        error!("Transaction has no from account");
+                        continue;
+                    }
+                };
+                let accout_nonce = self.ipc.eth().transaction_count(account, None).await;
+
+                match accout_nonce {
+                    Ok(accout_nonce) => {
+                        let mut buffer = buffer.lock().await;
+                        let bytes = serde_json::to_vec(&txn).unwrap();
+                        let committed_hash = TxnHash::new(simple_hash::hash_to_fixed_array(&bytes));
+                        let vtxn = VerifiedTxnWithAccountSeqNum {
+                            txn: VerifiedTxn {
+                                bytes,
+                                sender: covert_account(txn.from.unwrap()),
+                                sequence_number: txn.nonce.as_u64(),
+                                chain_id: ExternalChainId::new(0),
+                                committed_hash: committed_hash.into(),
+                            },
+                            account_seq_num: accout_nonce.as_u64(),
+                        };
+                        println!(
+                            "push txn nonce: {} acc_nonce: {}",
+                            txn.nonce, vtxn.account_seq_num
+                        );
+                        buffer.push(vtxn);
+                    }
+                    Err(e) => {
+                        error!("Failed to get nonce for account {:?} with {:?}", account, e);
+                    }
+                }
+            } else {
+                error!("Failed to get transaction {:?} {:?}", txn_hash, txn);
+            }
+        }
+        info!("end process pending transactions");
+        Ok(())
+    }
+}
+
+#[derive(Serialize)]
+struct JsonRpcRequest<'a> {
+    jsonrpc: &'a str,
+    method: &'a str,
+    params: Vec<String>,
+    id: u64,
+}
+
+#[derive(serde::Deserialize)]
+struct JsonRpcResponse<T> {
+    result: T,
+}
---
+++ bin/pethv2/src/reth_coordinator/mod.rs
@@ -0,0 +1,148 @@
+pub mod queue;
+pub mod state;
+use std::sync::Arc;
+
+use crate::reth_cli::RethCli;
+use api_types::u256_define::TxnHash;
+use api_types::{
+    u256_define::BlockId, u256_define::ComputeRes, ExecError, ExecTxn, ExecutionApiV2, ExternalBlock, ExternalBlockMeta,
+    ExternalPayloadAttr, VerifiedTxn, VerifiedTxnWithAccountSeqNum,
+};
+use async_trait::async_trait;
+use reth_payload_builder::PayloadId;
+use reth_primitives::B256;
+use state::State;
+use tokio::sync::mpsc;
+use tokio::sync::Mutex;
+use tracing::info;
+
+pub struct Buffer<T> {
+    sender: mpsc::Sender<T>,
+    receiver: Arc<Mutex<mpsc::Receiver<T>>>,
+}
+
+impl<T> Buffer<T> {
+    pub fn new(size: usize) -> Buffer<T> {
+        let (sender, receiver) = mpsc::channel(size);
+        Buffer { sender, receiver: Arc::new(Mutex::new(receiver)) }
+    }
+
+    pub async fn send(&self, item: T) {
+        self.sender.clone().send(item).await.unwrap();
+    }
+
+    pub async fn recv(&self) -> T {
+        let mut recv = self.receiver.lock().await;
+        recv.recv().await.unwrap()
+    }
+}
+
+pub struct RethCoordinator {
+    reth_cli: RethCli,
+    pending_buffer: Arc<Mutex<Vec<VerifiedTxnWithAccountSeqNum>>>,
+    state: Arc<Mutex<State>>,
+    queue: queue::Queue,
+    pending_payload_id: Buffer<PayloadId>,
+    // parent block id and payload id
+    commit_buffer: Buffer<(PayloadId, B256)>,
+}
+
+impl RethCoordinator {
+    pub fn new(reth_cli: RethCli) -> Self {
+        let mut state = State::new();
+        Self {
+            queue: queue::Queue::new(1),
+            reth_cli,
+            pending_buffer: Arc::new(Mutex::new(Vec::new())),
+            state: Arc::new(Mutex::new(state)),
+            pending_payload_id: Buffer::new(1),
+            commit_buffer: Buffer::new(1),
+        }
+    }
+
+    pub async fn run(&self) {
+        self.reth_cli.process_pending_transactions(self.pending_buffer.clone()).await.unwrap();
+    }
+}
+
+#[async_trait]
+impl ExecutionApiV2 for RethCoordinator {
+    async fn add_txn(&self, _bytes: ExecTxn) -> Result<TxnHash, ExecError> {
+        panic!("Reth Coordinator does not support add_txn");
+    }
+
+    async fn recv_unbroadcasted_txn(&self) -> Result<Vec<VerifiedTxn>, ExecError> {
+        panic!("Reth Coordinator does not support recv_unbroadcasted_txn");
+    }
+
+    async fn check_block_txns(
+        &self,
+        payload_attr: ExternalPayloadAttr,
+        txns: Vec<VerifiedTxn>,
+    ) -> Result<bool, ExecError> {
+        info!("check_block_txns with payload_attr: {:?}", payload_attr);
+        for txn in txns {
+            let mut state = self.state.lock().await;
+            let txn = serde_json::from_slice(&txn.bytes).unwrap();
+            if !state.check_new_txn(&payload_attr, txn) {
+                return Ok(false);
+            }
+        }
+        Ok(true)
+    }
+
+    async fn recv_pending_txns(&self) -> Result<Vec<VerifiedTxnWithAccountSeqNum>, ExecError> {
+        let mut buffer = self.pending_buffer.lock().await;
+        info!("recv_pending_txns with buffer size: {:?}", buffer.len());
+        let res = buffer.drain(..).collect();
+        Ok(res)
+    }
+
+    async fn send_ordered_block(
+        &self,
+        parent_id: BlockId,
+        mut ordered_block: ExternalBlock,
+    ) -> Result<(), ExecError> {
+        info!("send_ordered_block with parent_id: {:?}", parent_id);
+        self.queue.send_exec(ordered_block.block_meta.block_id).await;
+        let mut state = self.state.lock().await;
+        let parent_hash = state.get_block_hash(parent_id.clone());
+        if parent_hash.is_none() {
+            panic!("parent block not found {}", parent_id);
+        }
+        let parent_hash = parent_hash.unwrap();
+        ordered_block.txns = ordered_block
+            .txns
+            .into_iter()
+            .filter(|txn| state.update_account_seq_num(txn))
+            .collect();
+        let payload_id = self.reth_cli.push_ordered_block(ordered_block, parent_hash).await;
+        self.pending_payload_id.send(payload_id.clone().unwrap()).await;
+        self.commit_buffer.send((payload_id.unwrap(), parent_hash)).await;
+        Ok(())
+    }
+
+    async fn recv_executed_block_hash(
+        &self,
+        head: ExternalBlockMeta,
+    ) -> Result<ComputeRes, ExecError> {
+        info!("recv_executed_block_hash with head: {:?}", head);
+        self.queue.recv_exec().await;
+        self.queue.send_commit(head.block_id.clone()).await;
+        let block_id = &head.block_id;
+        let reth_block_id = B256::from_slice(&block_id.0);
+        let payload_id = self.pending_payload_id.recv().await;
+        let block_hash = self.reth_cli.process_payload_id(reth_block_id, payload_id).await;
+        self.state.lock().await.insert_new_block(head.block_id, block_hash.unwrap().into());
+        Ok(ComputeRes::new(block_hash.unwrap().into()))
+    }
+
+    async fn commit_block(&self, block_id: BlockId) -> Result<(), ExecError> {
+        info!("commit_block with block_id: {:?}", block_id);
+        let (payload_id, parent_hash) = self.commit_buffer.recv().await;
+        let block_hash = self.state.lock().await.get_block_hash(block_id).unwrap();
+        self.reth_cli.commit_block(parent_hash, payload_id, block_hash.into()).await.unwrap();
+        self.queue.recv_commit().await;
+        Ok(())
+    }
+}
