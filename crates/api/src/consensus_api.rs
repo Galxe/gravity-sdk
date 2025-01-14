ad9110f747d81bc725211782d517860302f10226
ByteYue <yj976240184@gmail.com>
2025-01-14 17:36:51 +0800

    m

--- crates/api/src/consensus_api.rs
+++ crates/api/src/consensus_api.rs
@@ -26,10 +26,6 @@ use aptos_telemetry::service::start_telemetry_service;
 use async_trait::async_trait;
 
 use aptos_types::chain_id::ChainId;
-use coex_bridge::{
-    call::{self, AsyncCallImplTrait},
-    get_coex_bridge, Func,
-};
 use futures::channel::mpsc;
 use tokio::runtime::Runtime;
 
@@ -58,67 +54,6 @@ fn fail_point_check(node_config: &NodeConfig) {
     } else if node_config.failpoints.is_some() {
         warn!("Failpoints is set in the node config, but the binary didn't compile with this feature!");
     }
-}
-
-pub struct SendOrderedBlocksCall {
-    consensus_engine: Arc<ConsensusEngine>,
-}
-
-#[async_trait]
-impl AsyncCallImplTrait for SendOrderedBlocksCall {
-    type Input = ([u8; 32], ExternalBlock);
-    type Output = ();
-    async fn call(&self, input: Self::Input) -> Result<Self::Output, ()> {
-        Ok(self.consensus_engine.send_ordered_block(input.0, input.1).await)
-    }
-}
-
-pub struct RecvExecutedBlockHashCall {
-    consensus_engine: Arc<ConsensusEngine>,
-}
-
-#[async_trait]
-impl AsyncCallImplTrait for RecvExecutedBlockHashCall {
-    type Input = ExternalBlockMeta;
-    type Output = ComputeRes;
-    async fn call(&self, input: Self::Input) -> Result<Self::Output, ()> {
-        Ok(self.consensus_engine.recv_executed_block_hash(input).await)
-    }
-}
-
-pub struct CommitBlockHashCall {
-    consensus_engine: Arc<ConsensusEngine>,
-}
-
-#[async_trait]
-impl AsyncCallImplTrait for CommitBlockHashCall {
-    type Input = [u8; 32];
-    type Output = ();
-    async fn call(&self, input: Self::Input) -> Result<Self::Output, ()> {
-        Ok(self.consensus_engine.commit_block_hash(input).await)
-    }
-}
-
-fn register_hook_func(consensus_engine: Arc<ConsensusEngine>) {
-    let coex_bridge = get_coex_bridge();
-    coex_bridge.register(
-        "send_ordered_block".to_string(),
-        Func::SendOrderedBlocks(Arc::new(call::AsyncCall::new(Box::new(SendOrderedBlocksCall {
-            consensus_engine: consensus_engine.clone(),
-        })))),
-    );
-    coex_bridge.register(
-        "recv_executed_block_hash".to_string(),
-        Func::RecvExecutedBlockHash(Arc::new(call::AsyncCall::new(Box::new(RecvExecutedBlockHashCall {
-            consensus_engine: consensus_engine.clone(),
-        })))),
-    );
-    coex_bridge.register(
-        "commit_block_hash".to_string(),
-        Func::CommittedBlockHash(Arc::new(call::AsyncCall::new(Box::new(CommitBlockHashCall {
-            consensus_engine,
-        })))),
-    );
 }
 
 impl ConsensusEngine {
@@ -264,7 +199,7 @@ impl ConsensusEngine {
             execution_layer: execution_layer.clone(),
             runtimes,
         });
-        register_hook_func(arc_consensus_engine.clone());
+        crate::coex::register_hook_func(arc_consensus_engine.clone());
         // process new round should be after init retƒh hash
         let _ = event_subscription_service.notify_initial_configs(1_u64);
         arc_consensus_engine
