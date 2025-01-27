a02725ae9ded504b17e7e04e230d41074d6a930f
ByteYue <yj976240184@gmail.com>
2025-01-27 14:40:39 +0800

    split bash

--- deploy_utils/ansible/manager.sh
+++ deploy_utils/ansible/manager.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 action=<deploy|stop> node=<server1,server2,...>"
+    echo "Usage: $0 action=<deploy|stop> node=<node1,node2,...>"
     exit 1
 fi
 
@@ -15,7 +15,7 @@ for arg in "$@"; do
         ;;
     *)
         echo "Unknown parameter: $arg"
-        echo "Usage: $0 action=<deploy|stop> node=<server1,server2,...>"
+        echo "Usage: $0 action=<deploy|stop> node=<node1,node2,...>"
         exit 1
         ;;
     esac
@@ -55,6 +55,18 @@ if [ $? -eq 0 ]; then
 else
     echo "Ansible playbook execution failed."
     exit 1
+fi
+
+if [ "$action" == "deploy" ]; then
+    echo "Starting peth on nodes=$node..."
+    ansible-playbook -i inventory.ini reth_start.yml --limit "$node"
+
+    if [ $? -eq 0 ]; then
+        echo "Ansible playbook executed successfully."
+    else
+        echo "Ansible playbook execution failed."
+        exit 1
+    fi
 fi
 
 validator_file="./server_confs/${node}/genesis/validator.yml"
