084a149ca3d2c44c7e22d15694d47456fce9dd01
ByteYue <yj976240184@gmail.com>
2025-01-28 15:09:02 +0800

    a

--- aptos-core/network/framework/src/application/storage.rs
+++ aptos-core/network/framework/src/application/storage.rs
@@ -17,7 +17,7 @@ use aptos_config::{
     network_id::{NetworkId, PeerNetworkId},
 };
 use aptos_infallible::{Mutex, RwLock};
-use aptos_logger::{info, sample::{self, SampleRate}, warn};
+use aptos_logger::{sample, sample::SampleRate, info, warn};
 use aptos_peer_monitoring_service_types::PeerMonitoringMetadata;
 use aptos_types::{account_address::AccountAddress, PeerId};
 use arc_swap::ArcSwap;
