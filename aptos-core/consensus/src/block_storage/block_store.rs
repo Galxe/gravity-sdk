66cd12f199875c6434b74c04b0a9672f9a532ed5
Lightman <915311741@qq.com>
2025-02-26 15:58:21 +0800

    tmp

--- aptos-core/consensus/src/block_storage/block_store.rs
+++ aptos-core/consensus/src/block_storage/block_store.rs
@@ -190,6 +190,7 @@ impl BlockStore {
                         self.payload_manager.get_transactions(block_to_recover.block()).await
                     {
                         info!("recover block {}, txn_size: {}", block_to_recover.block(), txns.len());
+                        info!("recover {} txn_size: {}", block_to_recover.block().id(), txns.len());
                         let verified_txns: Vec<VerifiedTxn> =
                             txns.iter().map(|txn| txn.into()).collect();
                         let txn_num = verified_txns.len() as u64;
