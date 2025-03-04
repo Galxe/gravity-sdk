08b7c689c82917dce6e91004da1feb5ac459e57b
Lightman <915311741@qq.com>
2025-03-04 17:36:49 +0800

    [bugfix] Fix coredump because execute same block twice

--- bin/gravity_node/src/reth_coordinator/state.rs
+++ bin/gravity_node/src/reth_coordinator/state.rs
@@ -1,7 +1,11 @@
+use std::sync::atomic::{AtomicU64, Ordering};
+
 use api_types::account::ExternalAccountAddress;
 use api_types::VerifiedTxn;
 use api_types::{u256_define::BlockId, ExternalPayloadAttr};
 use greth::reth::primitives::B256;
+use greth::reth_primitives::revm_primitives::bitvec::store::BitStore;
+use greth::reth_primitives::revm_primitives::bitvec::view::BitViewSized;
 use greth::reth_primitives::Transaction;
 use tracing::debug;
 pub struct BuildingState {
@@ -21,10 +25,13 @@ pub struct State {
     block_hash_to_id: std::collections::HashMap<B256, BlockId>,
     block_id_parent: std::collections::HashMap<BlockId, BlockId>,
     building_block: std::collections::HashMap<ExternalPayloadAttr, BuildingState>,
+    block_id_to_block_number: std::collections::HashMap<BlockId, u64>,
+    latest_executed_block_number: u64,
+    latest_committed_block_number: u64,
 }
 
 impl State {
-    pub fn new() -> Self {
+    pub fn new(latest_block_numnber: u64) -> Self {
         State {
             status: Status::Executing,
             accout_seq_num: std::collections::HashMap::new(),
@@ -32,12 +39,15 @@ impl State {
             block_hash_to_id: std::collections::HashMap::new(),
             block_id_parent: std::collections::HashMap::new(),
             building_block: std::collections::HashMap::new(),
+            block_id_to_block_number: std::collections::HashMap::new(),
+            latest_executed_block_number: latest_block_numnber,
+            latest_committed_block_number: latest_block_numnber,
         }
     }
 
     pub fn update_account_seq_num(&mut self, txn: &VerifiedTxn) -> bool {
         let account = txn.sender.clone();
-        let seq_num = self.accout_seq_num.entry(account).or_insert(txn.sequence_number as i64 -1);
+        let seq_num = self.accout_seq_num.entry(account).or_insert(-1);
         if *seq_num + 1 != txn.sequence_number as i64 {
             debug!("meet false seq_num: {:?} {:?}", seq_num, txn.sequence_number);
             return false;
@@ -64,5 +74,39 @@ impl State {
 
     pub fn get_block_hash(&self, block_id: BlockId) -> Option<B256> {
         self.block_id_to_hash.get(&block_id).cloned()
+    }
+
+    pub fn insert_block_number(&mut self, block_id: BlockId, block_numnber: u64) {
+        self.block_id_to_block_number.insert(block_id, block_numnber);
+    }
+
+    pub fn get_block_number(&self, block_id: &BlockId) -> u64 {
+        self.block_id_to_block_number.get(block_id).unwrap().clone()
+    }
+
+    pub fn cas_executed_block_number(&mut self, executed_block_number: u64) -> bool {
+        assert!(executed_block_number <= self.latest_executed_block_number + 1);
+        if executed_block_number == self.latest_executed_block_number + 1 {
+            self.latest_executed_block_number += 1;
+            return true;
+        }
+        false
+    }
+
+    pub fn cas_committed_block_number(&mut self, committed_block_number: u64) -> bool {
+        assert!(committed_block_number <= self.latest_committed_block_number + 1);
+        if committed_block_number == self.latest_committed_block_number + 1 {
+            self.latest_committed_block_number += 1;
+            return true;
+        }
+        false
+    }
+
+    pub fn latest_executed_block_number(&self) -> u64 {
+        self.latest_executed_block_number
+    }
+
+    pub fn latest_committed_block_number(&self) -> u64 {
+        self.latest_committed_block_number
     }
 }
