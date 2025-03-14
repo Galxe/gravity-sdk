9c15f7594152e81b4040982889281ee161ef4156
keanji-x <your_email@example.com>
2025-03-14 15:59:51 +0800

    fix kv

--- aptos-core/consensus/src/block_storage/block_store.rs
+++ aptos-core/consensus/src/block_storage/block_store.rs
@@ -363,18 +363,22 @@ impl BlockStore {
                 commit_decision,
             );
         } else {
-            // let mut blocks = vec![];
-            // for p_block in &blocks_to_commit {
-            //     if let Some(_) = p_block.block().block_number() {
-            //         continue;
-            //     }
-            //     p_block.block().set_block_number(self.storage.fetch_next_block_number());
-            //     blocks.push(p_block.as_ref().into());
-            // }
-
-            // if blocks.len() != 0 {
-            //     self.storage.save_tree(blocks, vec![]).unwrap();
-            // }
+            let mut blocks = vec![];
+            for p_block in &blocks_to_commit {
+                if let Some(_) = p_block.block().block_number() {
+                    continue;
+                }
+                p_block.block().set_block_number(self.storage.fetch_next_block_number());
+                blocks.push(p_block.as_ref().into());
+            }
+            let storage_clone = self.storage.clone();
+            tokio::spawn(async move {
+                if blocks.len() != 0 {
+                    storage_clone.save_tree(blocks, vec![]).unwrap();
+                }
+            });
+            
+            
             // This callback is invoked synchronously with and could be used for multiple batches of blocks.
             self.execution_client
                 .finalize_order(
