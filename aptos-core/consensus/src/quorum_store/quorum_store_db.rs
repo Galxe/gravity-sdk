dd4b26ea9cbc9f75c230919fb542166f071e9f41
keanji-x <jianxie0@gmail.com>
2025-05-30 14:16:44 +0800

    remove perisist

--- aptos-core/consensus/src/quorum_store/quorum_store_db.rs
+++ aptos-core/consensus/src/quorum_store/quorum_store_db.rs
@@ -85,7 +85,8 @@ impl QuorumStoreStorage for QuorumStoreDB {
             batch.digest(),
             batch.expiration()
         );
-        Ok(self.db.put::<BatchSchema>(batch.digest(), &batch)?)
+        // Ok(self.db.put::<BatchSchema>(batch.digest(), &batch)?)
+        Ok(())
     }
 
     fn get_batch(&self, digest: &HashValue) -> Result<Option<PersistedValue>, DbError> {
