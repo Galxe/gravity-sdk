4f8a473f16cd3517056150b347628997a3da710a
ByteYue <yj976240184@gmail.com>
2025-05-30 11:09:40 +0800

    use new config storage of gaptos

--- bin/gravity_node/src/reth_cli.rs
+++ bin/gravity_node/src/reth_cli.rs
@@ -14,6 +14,7 @@ use gaptos::api_types::{
     ExecutionBlocks, ExternalBlock, VerifiedTxn, VerifiedTxnWithAccountSeqNum,
     GLOBAL_CRYPTO_TXN_HASHER,
 };
+use gaptos::api_types::config_storage::{ConfigStorage, OnChainConfig, OnChainConfigResType};
 
 use greth::{
     gravity_storage::block_view_storage::BlockViewStorage,
@@ -457,5 +458,20 @@ impl RethCli {
                 .await
                 .unwrap();
         }
+    }
+}
+pub struct RethCliConfigStorage {
+    reth_cli: Arc<RethCli>,
+}
+
+impl RethCliConfigStorage {
+    pub fn new(reth_cli: Arc<RethCli>) -> Self {
+        Self { reth_cli }
+    }
+}
+
+impl ConfigStorage for RethCliConfigStorage {
+    fn fetch_config_bytes(&self, config_name: OnChainConfig, block_number: u64) -> Option<OnChainConfigResType> {
+        self.reth_cli.pipe_api.fetch_config_bytes(config_name, block_number)
     }
 }
