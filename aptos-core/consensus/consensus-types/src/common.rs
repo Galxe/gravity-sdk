58871335f0d890cb595c0c319015bae9a0852adb
Lightman <915311741@qq.com>
2025-07-19 20:17:20 +0800

    tmp

--- aptos-core/consensus/consensus-types/src/common.rs
+++ aptos-core/consensus/consensus-types/src/common.rs
@@ -569,6 +569,7 @@ impl CryptoHash for BatchPayload {
         // state.update(&bytes);
         // state.finish()
         for txn in &self.txns {
+            info!("lightman0719 committed_hash {}", txn.committed_hash())
             state.update(txn.committed_hash().as_ref());
         }
         state.finish()
