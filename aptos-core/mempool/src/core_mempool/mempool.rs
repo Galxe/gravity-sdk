e18feae7de3e43c70427b25ed6407ef0d5032902
Acat <jianxie0@gmail.com>
2025-10-24 11:35:31 +0800

    refactor: remove useless mempool (#421)

--- aptos-core/mempool/src/core_mempool/mempool.rs
+++ aptos-core/mempool/src/core_mempool/mempool.rs
@@ -5,18 +5,12 @@
 //! Mempool is used to track transactions which have been submitted but not yet
 //! agreed upon.
 use crate::{
-    core_mempool::{
-        index::TxnPointer,
-        transaction::{InsertionInfo, MempoolTransaction, TimelineState},
-        transaction_store::TransactionStore,
-    },
-    logging::{LogEntry, LogSchema, TxnsLog},
+    core_mempool::transaction::TimelineState,
     network::BroadcastPeerPriority,
     shared_mempool::types::{
         MempoolSenderBucket, MultiBucketTimelineIndexIds, TimelineIndexIdentifier,
     },
 };
-use aptos_consensus_types::common::{TransactionInProgress, TransactionSummary};
 use gaptos::aptos_config::config::NodeConfig;
 use gaptos::aptos_crypto::HashValue;
 use gaptos::aptos_logger::prelude::*;
@@ -28,33 +22,28 @@ use gaptos::aptos_types::{
 };
 use gaptos::{
     api_types::{
-        account::ExternalAccountAddress, u256_define::TxnHash, VerifiedTxnWithAccountSeqNum,
+        account::ExternalAccountAddress, u256_define::TxnHash,
     },
     aptos_mempool::shared_mempool::types::CoreMempoolTrait,
 };
 use std::{
     collections::{BTreeMap, HashMap, HashSet},
-    sync::{atomic::Ordering, Arc, Mutex},
-    time::{Duration, Instant, SystemTime},
+    sync::{Arc, Mutex},
+    time::{Duration, Instant},
 };
 
 use super::transaction::VerifiedTxn;
 use block_buffer_manager::TxPool;
-use gaptos::aptos_mempool::counters;
 
 pub struct TxnCache {
     old_cache: HashSet<TxnHash>,
     cache: HashSet<TxnHash>,
-    size: usize
+    size: usize,
 }
 
 impl TxnCache {
     fn new(size: usize) -> Self {
-        Self {
-            old_cache: HashSet::new(),
-            cache: HashSet::new(),
-            size,
-        }
+        Self { old_cache: HashSet::new(), cache: HashSet::new(), size }
     }
 
     fn insert(&mut self, txn_hash: TxnHash) {
@@ -68,7 +57,6 @@ impl TxnCache {
     fn is_contains(&self, txn_hash: &TxnHash) -> bool {
         self.cache.contains(txn_hash)
     }
-
 }
 
 pub struct Mempool {
@@ -76,7 +64,6 @@ pub struct Mempool {
     pool: Box<dyn TxPool>,
     txn_cache: Arc<Mutex<TxnCache>>,
 }
-
 
 impl CoreMempoolTrait for Mempool {
     fn timeline_range(
@@ -117,15 +104,12 @@ impl CoreMempoolTrait for Mempool {
         let iter = self.pool.get_broadcast_txns(Some(filter));
         let mut broacasted_txns = vec![];
         let mut visited_cache = self.txn_cache.lock().unwrap();
-        for txn in iter {   
+        for txn in iter {
             visited_cache.insert(TxnHash::from_bytes(txn.committed_hash().as_slice()));
-            broacasted_txns.push((
-                VerifiedTxn::from(txn).into(),
-                0,
-            ));
+            broacasted_txns.push((VerifiedTxn::from(txn).into(), 0));
         }
         let len = broacasted_txns.len();
-        
+
         (broacasted_txns, MultiBucketTimelineIndexIds { id_per_bucket: vec![0; len] })
     }
 
@@ -153,9 +137,7 @@ impl CoreMempoolTrait for Mempool {
     ) -> MempoolStatus {
         info!("add_txn sequence_info: {}", txn.sequence_number());
         let verfited_txn = crate::core_mempool::transaction::VerifiedTxn::from(txn);
-        let res = self.pool.add_external_txn(
-            verfited_txn.into(),
-        );
+        let res = self.pool.add_external_txn(verfited_txn.into());
         if res {
             MempoolStatus::new(MempoolStatusCode::Accepted)
         } else {
@@ -207,10 +189,7 @@ impl CoreMempoolTrait for Mempool {
 
 impl Mempool {
     pub fn new(_config: &NodeConfig, pool: Box<dyn TxPool>) -> Self {
-        Self {
-            pool,
-            txn_cache: Arc::new(Mutex::new(TxnCache::new(100000))),
-        }
+        Self { pool, txn_cache: Arc::new(Mutex::new(TxnCache::new(100000))) }
     }
 
     /// This function will be called once the transaction has been stored.
@@ -224,43 +203,6 @@ impl Mempool {
         // self.transactions
         //     .commit_transaction(sender, sequence_number);
     }
-
-    pub(crate) fn reject_transaction(
-        &mut self,
-        _sender: &AccountAddress,
-        _sequence_number: u64,
-        _hash: &HashValue,
-        _reason: &DiscardedVMStatus,
-    ) {
-    }
-
-    pub(crate) fn log_txn_latency(
-        _insertion_info: &InsertionInfo,
-        _bucket: &str,
-        _stage: &'static str,
-        _priority: &str,
-    ) {
-    }
-
-    pub(crate) fn log_commit_transaction(
-        &self,
-        _sender: &AccountAddress,
-        _sequence_number: u64,
-        _tracked_use_case: Option<(UseCaseKey, &String)>,
-        _block_timestamp: Duration,
-    ) {
-    }
-
-    pub(crate) fn add_user_txns_batch(
-        &mut self,
-        _txns_with_numbers: Vec<VerifiedTxnWithAccountSeqNum>,
-        _client_submitted: bool,
-        _timeline_state: TimelineState,
-        _priority: Option<BroadcastPeerPriority>,
-    ) -> Vec<MempoolStatus> {
-        panic!()
-    }
-
     /// Used to add a transaction to the Mempool.
     /// Performs basic validation: checks account's sequence number.
     pub(crate) fn send_user_txn(
@@ -314,22 +256,6 @@ impl Mempool {
             }
         }
         transactions
-    }
-
-    pub(crate) fn get_txn_count(&self) -> usize {
-        // self.transactions.txn_size()
-        todo!()
-    }
-
-    pub(crate) fn priority_index_size(&self) -> usize {
-        // self.transactions.priority_index_size()
-        todo!()
-    }
-
-    #[cfg(test)]
-    pub fn get_transaction_store(&self) -> &TransactionStore {
-        // &self.transactions
-        panic!()
     }
 
     pub fn gen_snapshot(&self) -> Vec<SignedTransaction> {
