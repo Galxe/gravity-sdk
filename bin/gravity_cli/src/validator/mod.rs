05d6116b80399c61673a1be120d65f079e8b3736
nekomoto911 <nekomoto911@gmail.com>
2025-12-24 18:05:51 +0800

    feat(cli): support validator list for gravity_cli (#476)
    ```
    Usage: gravity_cli validator list --rpc-url <RPC_URL>
    
    Options:
          --rpc-url <RPC_URL>  RPC URL for gravity node
      -h, --help               Print help
    ```

--- bin/gravity_cli/src/validator/mod.rs
+++ bin/gravity_cli/src/validator/mod.rs
@@ -1,12 +1,14 @@
 mod contract;
 mod join;
 mod leave;
+mod list;
 mod util;
 
 use clap::{Parser, Subcommand};
 
 use crate::validator::join::JoinCommand;
 use crate::validator::leave::LeaveCommand;
+use crate::validator::list::ListCommand;
 
 #[derive(Debug, Parser)]
 pub struct ValidatorCommand {
@@ -18,5 +20,6 @@ pub struct ValidatorCommand {
 pub enum SubCommands {
     Join(JoinCommand),
     Leave(LeaveCommand),
+    List(ListCommand),
     // TODO: other commands
 }
