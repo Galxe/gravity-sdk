769981d6a44f5c2de8d2d385e136e49a1b8bfe06
Lightman <915311741@qq.com>
2026-01-05 09:24:59 +0800

    tmp

--- aptos-core/consensus/src/block_storage/block_store.rs
+++ aptos-core/consensus/src/block_storage/block_store.rs
@@ -220,21 +220,18 @@ impl BlockStore {
         let last_ledger_info = self.storage.consensus_db().ledger_db.metadata_db().get_latest_ledger_info();
         info!("recover blocks, last_ledger_info: {:?}", last_ledger_info);
         certs.sort_unstable_by_key(|qc| qc.commit_info().round());
-        for qc in certs {
-            if qc.commit_info().round() > self.commit_root().round() {
-                if let Some(last_ledger_info) = &last_ledger_info {
-                    if last_ledger_info.ledger_info().epoch() == qc.commit_info().epoch()
-                        && qc.commit_info().round() <= last_ledger_info.commit_info().round()
-                    {
-                        info!(
-                            "sending qc {} to execution, current commit round {}",
-                            qc.commit_info().round(),
-                            self.commit_root().round()
-                        );
-                        if let Err(e) = self.send_for_execution(qc.into_wrapped_ledger_info(), true).await {
-                            error!("Error in try-committing blocks. {}", e.to_string());
-                            break;
-                        }
+        if let Some(last_ledger_info) = &last_ledger_info {
+            for qc in certs {
+                if qc.commit_info().round() > self.commit_root().round()
+                    && last_ledger_info.ledger_info().epoch() == qc.commit_info().epoch()
+                    && qc.commit_info().round() <= last_ledger_info.commit_info().round() {
+                    info!(
+                        "sending qc {} to execution, current commit round {}",
+                        qc.commit_info().round(),
+                        self.commit_root().round()
+                    );
+                    if let Err(e) = self.send_for_execution(qc.into_wrapped_ledger_info(), true).await {                            error!("Error in try-committing blocks. {}", e.to_string());
+                        break;
                     }
                 }
             }
